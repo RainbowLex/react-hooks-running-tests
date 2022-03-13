21a9c171dc7a07eff28c4b2970928596b0b7be69
Alexia Velador <alexiavel01@gmail.com>
2022-03-12 19:24:41 -0700

    commit tree 29c02dacad36e8967f3a0906773bea6c24cb2a7b

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -3,7 +3,6 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 
 import Article from "../components/Article";
-
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
